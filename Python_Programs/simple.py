eb086742b5148a3247cd8d3945f1de938f39a12d
KishoreRajesh <kishorerajesh007@gmail.com>
2023-08-15 16:09:49 +0530

    hi

---
+++ Python_Programs/simple.py
@@ -0,0 +1,23 @@
+ # print( "Hello Everyone!!!" )
+
+# print("    /\\")
+# print("   /  \\")
+# print("  /    \\")
+# print(" /      \\")
+# print("/________\\")
+
+# char_name ="kishore"
+# char_age = 18
+# print( "My Name is "+char_name+" And my Age is "+ str(char_age) +" . " )
+
+# char_name = "KISHORE"
+# print(char_name.lower())
+
+# user_name = input("Enter your Name :")
+# print( "Hello My Name is "+ user_name +"....." )
+
+# num_1 = input( "Enter 1st Number :" )
+# num_2 = input( "Enter 2nd Number :" )
+# result = float(num_1) + float(num_2)
+
+# print(result)
