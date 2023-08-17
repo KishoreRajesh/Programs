eb440f0240ea441f64b4ed010c63f8c8ac200d46
KishoreRajesh <kishorerajesh007@gmail.com>
2023-08-17 15:48:55 +0530

    co

---
+++ Python_Programs/classified.py
@@ -0,0 +1,6 @@
+class Student :
+    def __init__(self, name, reg_no, dept, gpa):
+        self.name = name
+        self.reg_no = reg_no
+        self.dept = dept
+        self.gpa = gpa
---
+++ Python_Programs/object_class.py
@@ -0,0 +1,5 @@
+from classified import Student
+    
+student1 = Student("kishore", 21218236, "mechanical", 9.8)
+
+print(student1.reg_no)
