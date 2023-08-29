8a0a4469106a04f0d103969298d7d43ba015e1a3
KishoreRajesh <kishorerajesh007@gmail.com>
2023-08-29 15:22:43 +0530

    d

---
+++ C++ Programs/basicCalculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float num1, num2;
+
+    cout << "Enter first Number :";
+    cin >> num1;
+    cout << "Enter second Number ";
+    cin >> num2;
+    cout << num1 + num2;
+
+    return 0;   
+}
