cd96755f0842050207e9eb3ddbea1d9c3421754e
KishoreRajesh <kishorerajesh007@gmail.com>
2023-08-29 13:14:37 +0530

    b

--- Python_Programs/mcq.py
+++ Python_Programs/mcq.py
@@ -1,23 +1,23 @@
 from class_object import questions
 
-Questions = [
+Questions_prompt = [
     "What is My Name ?\n a)king\n b)kishore\n c)Raja\n\n",
     "what is My Age ?\n a)12\n b)15\n c)18\n\n",
     "What is my birth Year ?\n a)2005\n b)1997\n c)2000\n\n",
 ]
 
-Questions1 = [
-    questions(Questions[0],"b"),
-    questions(Questions[1],"c"),
-    questions(Questions[2],"a"),
+Questions= [
+    questions(Questions_prompt[0],"b"),
+    questions(Questions_prompt[1],"c"),
+    questions(Questions_prompt[2],"a"),
 ]
 
-def test(mcq):
+def test(questions):
     score = 0
-    for question in mcq:
-        answer = input(mcq)
+    for question in Questions:
+        answer = input(Questions_prompt)
         if answer == questions.answer:
             score += 1
-    print("You scored " + str(score) + "/" +str(len(Questions1)))
+    print("You scored " + str(score) + "/" +str(len(Questions)))
 
-test(Questions1)
+test(questions)
