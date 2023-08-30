ca365170aee887dfc599cb87cb53b1f96b8a17d1
KishoreRajesh <kishorerajesh007@gmail.com>
2023-08-30 14:05:56 +0530

    g

--- C++ Programs/exponent.cpp
+++ C++ Programs/exponent.cpp
@@ -11,7 +11,7 @@ int main(){
     for(int i =0; i<powNum; i++){
         answer = answer * baseNum;
     }
-    cout << answer << endl;
+    cout << "Answer is :" << answer << endl;
 
     return 0;
 }
