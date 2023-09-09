3341df002920a7a0f634381c7c85c80083ce0abc
KishoreRajesh <kishorerajesh007@gmail.com>
2023-09-09 14:03:27 +0530

    n

--- C++ Programs/set.cpp
+++ C++ Programs/set.cpp
@@ -4,15 +4,17 @@ using namespace std;
 
 int main(){
     set<int>mySet;
-    mySet.insert(1);
     mySet.insert(2);
-    mySet.insert(3);
     mySet.insert(4);
-    mySet.insert(5);
+    mySet.insert(6);
+    mySet.insert(8);
+    mySet.insert(10);
 
     for(auto it = mySet.begin(); it != mySet.end();it++){
         cout << *it << " ";
     }
+    cout << endl;
+ 
 
     return 0;
 }
