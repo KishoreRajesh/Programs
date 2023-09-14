0333809b5f62efdb26045e0dcc9a532c74f6ee61
KishoreRajesh <kishorerajesh007@gmail.com>
2023-09-14 16:52:57 +0530

    p

---
+++ C++ Programs/problem1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string mystring;
+    cout << "-------Converting lowercasing words into uppercase ------\n";
+    cout << "Enter a lowercase sentence :";
+    getline(cin,mystring);
+    for(int i =0; i < mystring.length();i++){
+        if(islower(mystring[i])){
+            mystring[i] = mystring[i]-32;
+        }
+    }
+    cout << "--------------------------------------------------------\n";
+    cout << "The uppercased sentence = " << mystring << endl;
+    cout << "---------------------------------------------------------\n";
+    return 0;
+}
