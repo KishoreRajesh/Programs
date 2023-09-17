52245b19f6f1d034db6636ffddc8adb5767b3e03
KishoreRajesh <kishorerajesh007@gmail.com>
2023-09-17 12:21:49 +0530

    r

---
+++ C++ Programs/udemyProblems/challenge1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int fNum;
+    cout << "Enter a number from 1 to 100 :";
+    cin >> fNum;
+    cout << "Amazing ! It is my favorite number too.."<< endl;
+    cout << "no really! " << fNum << " is my Favorite number too..." << endl;
+    return 0;
+}
