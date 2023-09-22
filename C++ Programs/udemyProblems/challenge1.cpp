fbdae52f0f9b53cedc4cc526058b9f01e669413d
KishoreRajesh <kishorerajesh007@gmail.com>
2023-09-22 16:18:55 +0530

    v

--- C++ Programs/udemyProblems/challenge1.cpp
+++ C++ Programs/udemyProblems/challenge1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int fNum;
+    int fNum (0);
     cout << "Enter a number from 1 to 100 :";
     cin >> fNum;
     cout << "Amazing ! It is my favorite number too.."<< endl;
