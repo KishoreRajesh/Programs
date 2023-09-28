525055ce55da8e3119c3c037164e64040c8a9ce5
KishoreRajesh <kishorerajesh007@gmail.com>
2023-09-28 15:52:58 +0530

    x

--- C++ Programs/udemyProblems/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-void meanList(vector<int> &m){
-//     if(m.size() == 0){
-//         cout << "Unable to calculate the mean " << endl;
-//     }else{
-//         cout << "The mean of the list is " << meanCalc(m) << endl;
-//     }
-
-// }
