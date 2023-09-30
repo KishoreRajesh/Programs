7639ea02f4aff6788665afd8d41f8336f1c1c4cd
KishoreRajesh <kishorerajesh007@gmail.com>
2023-09-30 13:12:03 +0530

    w

--- C++ Programs/udemyProblems/challenge8.cpp
+++ C++ Programs/udemyProblems/challenge8.cpp
@@ -1,26 +1,7 @@
 #include <iostream>
 using namespace std;
-
-void print(const int *const arr, size_t size) {
-    cout << "[ ";
-    for (size_t i = 0; i < size; i++) {
-        cout << *(arr + i) << " ";
-    }
-    cout << "]";
-}
-
-int *apply_all(const int *const arr1, size_t arr1Size, const int *const arr2, size_t arr2Size) {
-    int *arr3 = new int[arr1Size * arr2Size];
-    int k = 0;
-    for (size_t i = 0; i < arr2Size; ++i) {
-        for (size_t j = 0; j < arr1Size; ++j) {
-            arr3[k] = arr1[j] * arr2[i];
-            ++k;
-        }
-    }
-    return arr3;
-}
-
+void print(const int *const,size_t);
+int *apply_all(const int *const,size_t,const int *const,size_t);
 int main() {
     int *result = nullptr; 
     int arr1[] = {1, 2, 3, 4, 5};
@@ -33,6 +14,26 @@ int main() {
     result = apply_all(arr1, 5, arr2, 3);
     cout << "Result = ";
     print(result, 15);
+    cout << endl;
     delete[] result; 
     return 0;
+}
+int *apply_all(const int *const arr1, size_t arr1Size, const int *const arr2, size_t arr2Size) {
+    int *arr3 = new int[arr1Size * arr2Size];
+    int k = 0;
+    for (size_t i = 0; i < arr2Size; ++i) {
+        for (size_t j = 0; j < arr1Size; ++j) {
+            arr3[k] = arr1[j] * arr2[i];
+            ++k;
+        }
+    }
+    return arr3;
+}
+
+void print(const int *const arr, size_t size) {
+    cout << "[ ";
+    for (size_t i = 0; i < size; i++) {
+        cout << *(arr + i) << " ";
+    }
+    cout << "]";
 }
