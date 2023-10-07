81131f8d749bcaf8f6bcb2acc51f63f1186ad024
KishoreRajesh <kishorerajesh007@gmail.com>
2023-10-07 12:04:56 +0530

    za

---
+++ C++ Programs/C++practical/personalInfo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << " Name          : kishore R" << endl;
+    cout << " Reg no        : 231134" << endl;
+    cout << " Year/Sem      : II/III" << endl;
+    cout << " Date Of Birth : 07/06/2005" << endl;
+    cout << " Dept          : Btech.Cse" << endl;
+    cout << " College name  : Sri Manakula Vinayagar Engineering College (An Atonomous Institution)" << endl;
+    return 0;
+}
