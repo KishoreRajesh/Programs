7fcd6e78ee744dcd8761efbd525aba34616da347
KishoreRajesh <kishorerajesh007@gmail.com>
2024-04-04 15:55:31 +0530

    done

--- C Programs/DAA_Practical/knapsack.c
+++ C Programs/DAA_Practical/knapsack.c
@@ -1,45 +1,55 @@
 #include<stdio.h>
-#include<stdlib.h>
-typedef struct{
-	int weight, profit;
-	float value;
-}item;
-
-int compare(const void* a, const void* b){
-	item* aa = (item*) a;
-	item* ab = (item*) b;
-	return (ab->value - aa->value);
+#define N 3
+struct knapsack{
+    float weight;
+    float profit;
+    float ratio;
+};
+struct knapsack k[N];
+void sort()
+{
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N-i-1;j++)
+        {
+            if(k[j].ratio<k[j+1].ratio)
+            {
+                struct knapsack temp = k[j];
+                k[j]=k[j+1];
+                k[j+1]=temp;
+            }
+        }
+    }
 }
 
-int main(){
-	item items[] = {{20, 60, 3}, {12, 40, 2.5}, {10, 20, 2}, {30, 100, 3.33}};
-	int n = 4;
-	int W = 50;
-	int i, j, count;
-	item* items_copy = (item*) malloc(n * sizeof(item));
-
-	for(i = 0; i < n; i++){
-		items_copy[i] = items[i];
-	}
-
-	qsort(items_copy, n, sizeof(item), compare);
-
-	int amount[n];
-	count = 0;
-	for(i = 0; i < n; i++){
-		if(items_copy[i].weight <= W){
-			amount[count] = items_copy[i].profit;
-			W -= items_copy[i].weight;
-			count++;
-		}
-	}
-
-	float total_profit = 0;
-	for(i = 0; i < count; i++){
-		total_profit += amount[i];
-	}
-
-	printf("Maximum profit is %.2f\n", total_profit);
-
-	return 0;
+int main()
+{
+    float cap = 20;
+    float p=0;
+    float current_cap=0;
+    for(int i=0;i<N;i++)
+    {
+        printf("Enter weight:");
+        scanf("%f",&k[i].weight);
+        printf("Enter profit:");
+        scanf("%f",&k[i].profit);
+        k[i].ratio=k[i].profit/k[i].weight;
+    }
+    sort();
+    int h=0;
+    while(current_cap<cap)
+    {
+        if((current_cap+k[h].weight)<=cap)
+        {
+            current_cap+=k[h].weight;
+            p+=k[h].profit;
+            h++;
+        }
+        else{
+            float t = cap - current_cap;
+            p+=(t/k[h].weight)*k[h].profit;
+            break;
+        }
+    }
+    printf("Maximum profit = %f",p);
 }
