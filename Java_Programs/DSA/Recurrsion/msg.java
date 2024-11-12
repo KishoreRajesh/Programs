7c6c5d14f7cb90e5f0d7e6c99abfa50cc672e388
KishoreRajesh <kishorerajesh007@gmai.com>
2024-11-12 15:43:32 +0530

    rte

---
+++ Java_Programs/DSA/Recurrsion/msg.java
@@ -0,0 +1,11 @@
+package DSA.Recurrsion;
+
+public class msg {
+    public static void main(String args[]){
+        message();
+    }
+
+public static void message(){
+    System.out.println("Hello");
+}
+}
