1b055e36c21d648867abad82a48f41bfe29e86f2
KishoreRajesh <kishorerajesh007@gmai.com>
2024-12-29 19:03:13 +0530

    true

--- Java_Programs/split/tempCodeRunnerFile.java
+++
@@ -1,7 +0,0 @@
-class tempCodeRunnerFile {
-    public static void main(String[] args) {
-
-      String operatingSystem = System.getProperty("os.name");
-      System.out.println(operatingSystem);
-    }
-  } 
